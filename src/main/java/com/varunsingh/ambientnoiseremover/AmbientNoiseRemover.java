f13d96a12118b9725ce4970219d61172dd423c23
Varun <31084854+Borumer@users.noreply.github.com>
2021-01-31 14:41:14 -0500

    Implement AmbientNoiseRemover * Begin implementing WaveFormTest * Begin implementing WaveForm class * Implement AmbientNoiseRemover

--- src/main/java/com/varunsingh/ambientnoiseremover/AmbientNoiseRemover.java
+++ src/main/java/com/varunsingh/ambientnoiseremover/AmbientNoiseRemover.java
@@ -1,13 +1,16 @@
 package com.varunsingh.ambientnoiseremover;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
-import javax.sound.sampled.*;
-import javax.sound.sampled.spi.*;
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
  * Samples: Talk by Neil Cummings at Kunsthal Aarhus, on 11 December, as part of
@@ -19,60 +22,84 @@ import javax.sound.sampled.spi.*;
  *          once
  */
 public class AmbientNoiseRemover {
-    private AudioInputStream audio;
-    private AudioFormat format;
-    private Clip sound;
+    private File sourceFile;
+    private File destinationFile;
+    private WaveForm waveForm;
 
-    public AmbientNoiseRemover(AudioInputStream f) {
-        try {
-            audio = AudioSystem.getAudioInputStream(f);
-            format = audio.getFormat();
-        } catch (UnsupportedAudioFileException | IOException e) {
-            e.printStackTrace();
-        }
-
-        sound = getSound();
+    public AmbientNoiseRemover(String src, String dest) throws UnsupportedAudioFileException, IOException {
+        sourceFile = new File(src);
+        destinationFile = new File(dest);
+        waveForm = new WaveForm(loadSourceFileData());
     }
 
     public static void main(String[] args) {
         try {
-            String[] samples = { "sample1.mp3", "sample2.mp3", "sample3.mp3", "sample4.mp3" };
-            for (String samplePath : samples) {
-                new AmbientNoiseRemover(getStreamFromFile(samplePath));
+            for (int i = 1; i <= 5; i++) {
+                String fileName = "sample" + i + ".wav";
+                String sourcePath = "data/samples/".concat(fileName);
+                String destPath = "data/output/".concat(fileName);
+
+                AmbientNoiseRemover anr = new AmbientNoiseRemover(sourcePath, destPath);
+                anr.writeOutputToFile();
+                // anr.playAudio();
             }
         } catch (UnsupportedAudioFileException | IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static AudioInputStream getStreamFromFile(String dataPath)
-            throws UnsupportedAudioFileException, IOException {
-        File file = new File("data/" + dataPath);
-        return AudioSystem.getAudioInputStream(file);
+    byte[] loadSourceFileData() throws UnsupportedAudioFileException {
+        try (AudioInputStream in = getAudioInputStream()) {
+            return in.readAllBytes();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new byte[] {};
+        }
     }
 
-    Clip getSound() {
-        DataLine.Info info = new DataLine.Info(Clip.class, format);
-        if (!AudioSystem.isLineSupported(info)) {
-            System.out.println("The line is not supported");
-            return null;
-        }
+    AudioInputStream getAudioInputStream() throws IOException, UnsupportedAudioFileException {
+        return AudioSystem.getAudioInputStream(sourceFile);
+    }
 
+    public WaveForm getWaveForm() {
+        return waveForm;
+    }
+
+    public void setWaveForm(WaveForm waveForm) {
+        this.waveForm = waveForm;
+    }
+
+    void playAudio() {
         try {
-            return (Clip) AudioSystem.getLine(info);
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(destinationFile));
+
+            clip.start();
+            while (!clip.isRunning())
+                Thread.sleep(10);
+            while (clip.isRunning())
+                Thread.sleep(10);
+
+            clip.close();
         } catch (LineUnavailableException e) {
             e.printStackTrace();
-            return null;
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    /**
-     * Inserts in the inverse sound wave at specified position
-     * 
-     * @return
-     */
-    Clip insertDestructiveInterference(int position) {
-        return null;
+    void writeOutputToFile() throws FileNotFoundException {
+        try (AudioInputStream in = getAudioInputStream()) {
+            AudioSystem.write(in, AudioFileFormat.Type.WAVE, new FileOutputStream(destinationFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        }
     }
 
 }
