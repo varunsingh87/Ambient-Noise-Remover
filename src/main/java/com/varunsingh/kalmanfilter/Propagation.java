4aae9ef1435f5141339ff63c573ea147a7c9425d
Varun <31084854+Borumer@users.noreply.github.com>
2021-04-12 21:06:01 -0400

    Begin new design in kalmanfilter package * Implement Propagatable interface * Implement Updateable interface * Create bare Update and Propagation classes to implement respective interfaces

---
+++ src/main/java/com/varunsingh/kalmanfilter/Propagation.java
@@ -0,0 +1,32 @@
+package com.varunsingh.kalmanfilter;
+
+import com.varunsingh.linearalgebra.Matrix;
+import com.varunsingh.linearalgebra.Vector;
+
+public class Propagation implements Propagatable {
+
+    @Override
+    public Vector predictNextStateVector() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Matrix predictNextStateCovarianceMatrix() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Vector modelNextStateVectorWithNoise() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Matrix modelNextStateCovarianceMatrixWithNoise() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
---
+++ src/main/java/com/varunsingh/kalmanfilter/Update.java
@@ -0,0 +1,26 @@
+package com.varunsingh.kalmanfilter;
+
+import com.varunsingh.linearalgebra.Matrix;
+import com.varunsingh.linearalgebra.Vector;
+
+public class Update implements Updateable {
+    
+    @Override
+    public Vector mapStateVectorToMeasurementVector() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Matrix calculateObservabilityOfMeasurementModel() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Matrix calculateObservabilityOfMeasurementCovariance() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
