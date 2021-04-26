47b352e060a70df37440f0cdc81a0cb3ffd00494
Varun <31084854+Borumer@users.noreply.github.com>
2021-04-25 21:24:21 -0400

    Implement Update

--- src/main/java/com/varunsingh/kalmanfilter/Update.java
+++ src/main/java/com/varunsingh/kalmanfilter/Update.java
@@ -4,23 +4,34 @@ import com.varunsingh.linearalgebra.Matrix;
 import com.varunsingh.linearalgebra.Vector;
 
 public class Update implements Updateable {
-    
+    Matrix observation;
+
+    Vector stateVector;
+    Vector noise;
+
+    public Update() {
+        observation = calculateObservabilityOfMeasurementModel();
+    }
+
     @Override
     public Vector mapStateVectorToMeasurementVector() {
-        // TODO Auto-generated method stub
-        return null;
+        return observation
+            .times(stateVector)
+            .plus(noise)
+            .asColumnVector();
     }
 
     @Override
     public Matrix calculateObservabilityOfMeasurementModel() {
-        // TODO Auto-generated method stub
-        return null;
+        return new Matrix(new double[][] {
+            { 1, 0 },
+            { 0, 1 }
+        });
     }
 
     @Override
     public Matrix calculateObservabilityOfMeasurementCovariance() {
-        // TODO Auto-generated method stub
         return null;
     }
-    
+
 }
